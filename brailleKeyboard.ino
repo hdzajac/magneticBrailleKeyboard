ab8b20f954eeedf16ad359eb334ae63bda5141c9
hubz00 <hubert.zajac@outlook.com>
2019-02-05 17:59:54 +0100

    working writing

--- brailleKeyboard.ino
+++ brailleKeyboard.ino
@@ -9,6 +9,7 @@ int restTime = 5000;
 int maxWorkTime = 3000;
 int minTimeBetweenRuns = 300;
 int vibrationSetLength = 500;
+int maxWritingLength = 1000;
 
 
 
@@ -19,8 +20,8 @@ int confirmationRevert = false;
 
 
 
-int signalPulses[3] = {1, 3, 4};
-int signalPulsesLength[3] = {400, 4, 25};
+int signalPulses[4] = {1, 1, 6, 4};
+int signalPulsesLength[4] = {400, 400, 35, 25};
 int revertPulses[3] = {false, true, true};
 
 int letters[26][6] = {
@@ -53,7 +54,7 @@ int letters[26][6] = {
 };
 
 
-const int ESCSignal = 6;
+const int ESCSignal = 255;
 const int ConfirmationSignal = 5;
 const int PreReadingMode = 1;
 const int ReadingMode = 2;
@@ -70,7 +71,7 @@ const int strongVibrate = 255;
 const int magnetsNumber = 6;
 
 typedef struct {
-  int inputPin;
+  byte inputPin;
   int idleVal;
 } Hall;
 
@@ -85,13 +86,18 @@ typedef struct {
   bool overheated;
   bool reseting;
   float repellingDirection;
+  unsigned long startTime;
+  unsigned long currentTime;
+  int iteration;
+  bool done;
+  bool newPulse;
 } Magnet;
 
 Magnet magnets[magnetsNumber];
 
 int forwardPins[] = {2, 4, 6, 8, 10, 12};
 int backwardPins[] = {1, 3, 5, 7, 9, 11, 13};
-int hallInputPins[] = {A0, A1, A2, A3, A4, A5};
+byte hallInputPins[] = {A0, A1, A2, A3, A4, A5};
 
 
 
@@ -107,6 +113,11 @@ void setup() {
     h.inputPin = hallInputPins[i];
     pinMode(h.inputPin, INPUT);
     h.idleVal = analogRead(h.inputPin);
+    Serial.print(h.inputPin);
+    Serial.print("idle val: ");
+    Serial.println(h.idleVal);
+    m.sensor = h;
+
 
     m.forwardPin = forwardPins[i];
     m.backwardPin = backwardPins[i];
@@ -121,6 +132,11 @@ void setup() {
     m.firstRun = millis();
     m.isRunning = false;
     m.overheated = false;
+    m.currentTime = 0;
+    m.startTime = 0;
+    m.iteration = 0;
+    m.done = false;
+    m.newPulse = true;
 
     magnets[i] = m;
   }
@@ -131,55 +147,63 @@ void setup() {
 
 
 void loop() {
-  int val = -1;
+  int val = -2;
 
-  digitalWrite(6, 1);
+  if (Serial.available()) { // If data is available to read,
+    val = Serial.read(); // read it and store it in val
+    Serial.print("received val: ");
+    Serial.println(val);
 
-  digitalWrite(7, 0);
+    if (val == ESCSignal) {
+      Serial.print("ESC received");
+      mode = SetUpMode;
+    }
 
+    if (mode == SetUpMode) {
+      Serial.println("Waiting in set up mode");
+      if (val == PreReadingMode) { // If 1 was receidved
+        mode = PreReadingMode;
+        for (int i = 0; i < magnetsNumber; i++) {
+          setDirection(i, magnets[i].repellingDirection );
+        }
+        Serial.write(ConfirmationSignal);
+      }
+      else if (val == WritingMode) {
+        mode = WritingMode;
+      }
+      else {
+        return;
+      }
+      Serial.print("waiting in mode: ");
+      Serial.println(mode);
+      delay(10); // Wait 10 milliseconds for next reading
+    }
+    else if (mode == PreReadingMode) {
+      if (val >= 0 && val < 10) {
+        Serial.print("Reading mode, received signal pulse type: ");
+        Serial.println(val);
+        Serial.print("Lenght: ");
+        Serial.println(signalPulsesLength[val]);
+        Serial.print("Repeitions: ");
+        Serial.println(signalPulses[val]);
 
+        mode = ReadingMode;
+        signalType = val;
+      }
+    }
+    else if (mode == ReadingMode) {
+      if (val != -2) { // If data is available to read,
+        Serial.print("Reading mode, received char: ");
+        Serial.println((char)(val + 'a' - shift));
+        handleReading(val);
+      }
+      delay(10);
+    }
+  }
+  else if (mode == WritingMode) {
+    handleWriting();
+  }
 
-    
-//  if (Serial.available()) { // If data is available to read,
-//    val = Serial.read(); // read it and store it in val
-//  }
-//  if (mode == SetUpMode) {
-//    if (val == PreReadingMode) { // If 1 was receidved
-//      mode = PreReadingMode;
-//      for (int i = 0; i < magnetsNumber; i++) {
-//        setDirection(i, magnets[i].repellingDirection );
-//      }
-//      Serial.write(ConfirmationSignal);
-//    }
-//    else if (val == WritingMode) {
-//      mode = WritingMode;
-//    }
-//    else {
-//      return;
-//    }
-//    Serial.print("waiting in mode: ");
-//    Serial.println(mode);
-//    delay(10); // Wait 10 milliseconds for next reading
-//  }
-//  else if (mode == PreReadingMode) {
-//    if (val > 0 && val < 10) {
-//      Serial.print("Reading mode, received signal pulse type: ");
-//      Serial.println(val);
-//      mode = ReadingMode;
-//      signalType = val;
-//    }
-//  }
-//  else if (mode == ReadingMode) {
-//    if (val != -1) { // If data is available to read,
-//      Serial.print("Reading mode, received char: ");
-//      Serial.println((char)(val + 'a' - shift));
-//      handleReading(val);
-//    }
-//    delay(10);
-//  }
-//  else if (mode == WritingMode) {
-//    handleWriting();
-//  }
 }
 
 
@@ -205,40 +229,62 @@ void handleReading(int letter) {
 
 void handleWriting() {
   int activeMagnets[6] = {0, 0, 0, 0, 0, 0};
+  int activeMagnetsCounter = 0;
+  int writing = 0;
   int inputCompleteMagnets[6] = {0, 0, 0, 0, 0, 0};
+  unsigned long startTime = 0;
+  int timesUp = 0;
 
 
-  for (int magnet = 0; magnet < magnetsNumber; magnet++) {
-    int reading = readHall(magnet);
+  while (!(activeMagnetsCounter == 0 && writing == 1) && timesUp == 0)
+  {
+    for (int magnet = 0; magnet < magnetsNumber; magnet++) {
+      if (startTime != 0 && millis() - startTime > maxWritingLength) {
+        timesUp = 1;
+      }
+      int reading = readHall(magnet);
 
-    int base = magnets[magnet].sensor.idleVal;
-    int diff = base - reading;
-    diff = abs(diff);
 
-    if (diff < farReading) {
-      if (certain(magnet, 0, farReading)) {
-        if (magnets[magnet].reseting) {
-          magnets[magnet].reseting = false;
+      int base = magnets[magnet].sensor.idleVal;
+      int diff = base - reading;
+      diff = abs(diff);
+
+      if (diff < farReading) {
+        if (certain(magnet, 0, farReading)) {
+          if (magnets[magnet].reseting) {
+            magnets[magnet].reseting = false;
+            activeMagnetsCounter--;
+          }
+        }
+        continue;
+      }
+      else  if (diff < touchReading && !magnets[magnet].reseting) {
+        if (certain(magnet, strongReading, touchReading)) {
+          if (activeMagnets[magnet] == 0) {
+            activeMagnets[magnet] = 1;
+            activeMagnetsCounter++;
+          }
+          if (writing == 0) {
+            writing = 1;
+            startTime = millis();
+          }
+
         }
       }
-      continue;
-    }
-    else  if (diff < touchReading && !magnets[magnet].reseting) {
-      if (certain(magnet, strongReading, touchReading)) {
-        activeMagnets[magnet] = 1;
+      else {
+        if (!magnets[magnet].reseting) {
+          //        Serial.printf("%d 1 %lu\n", magnet, millis());
+          magnets[magnet].reseting = true;
+          inputCompleteMagnets[magnet] = 1;
+        }
       }
     }
-    else {
-      if (!magnets[magnet].reseting) {
-        //        Serial.printf("%d 1 %lu\n", magnet, millis());
-        magnets[magnet].reseting = true;
-        inputCompleteMagnets[magnet] = 1;
-      }
+    if (writing == 1) {
+      pulseMagnets(signalPulsesLength[signalType], signalPulses[signalType], revertPulses[signalType], activeMagnets);
+      pulseMagnets(confirmationPulseLength, confirmationPulseRepetitions, confirmationRevert, inputCompleteMagnets);
     }
   }
-
-  pulseMagnets(signalPulsesLength[signalType], signalPulses[signalType], revertPulses[signalType], activeMagnets);
-  pulseMagnets(confirmationPulseLength, confirmationPulseRepetitions, confirmationRevert, inputCompleteMagnets);
+  Serial.print(getLetter(inputCompleteMagnets) + 'a');
 }
 
 // ================================================
@@ -249,39 +295,50 @@ void pulseMagnets(int pulseLength, int repetitions, int revert, int *magnetsToRu
   unsigned long total = 0;
   int runPulse = 1;
   int alreadyStopped = false;
-  Serial.print("vibrating: ");
-  Serial.println(revert);
+  int allDone = 0;
 
-  for (int i = 0; i < repetitions; i++) {
-    int pulse = 0;
-
-    while (pulse < pulseLength) {
-      unsigned long StartTime = millis();
-
-      for (int magnet = 0; magnet <  magnetsNumber; magnet++) {
-        if (magnetsToRun[magnet] == 1)
-        {
-          if (runPulse == 1) {
-            driveMagnet(magnet);
-            alreadyStopped = false;
-          }
-          else if (!alreadyStopped) {
-            stopMagnet(magnet);
-            alreadyStopped = true;
-          }
+  for (int i = 0; i < magnetsNumber; i++) {
+    if (magnetsToRun[i] == 1) {
+      allDone++;
+      (&magnets[i])->done = false;
+      (&magnets[i])->iteration = 0;
+    }
+  }
+  int i = 0;
+  while (allDone != 0) {
+    Magnet *m = &magnets[i];
+    if (magnetsToRun[i] == 1 && !m->done) {
+      if (m->newPulse) {
+        m->newPulse = false;
+        m->startTime = millis();
+        if (revert) {
+          revertDirection(i);
+        }
+        if (!revert && m->iteration % 2 == 1) {
+          continue;
+        }
+        else {
+          driveMagnet(i);
         }
       }
-      unsigned long CurrentTime = millis();
-      pulse += CurrentTime - StartTime;
-    }
-    if (revert) {
-      for (int magnet = 0; magnet < magnetsNumber; magnet++) {
-        revertDirection(magnet);
+      else if ((millis() - m->startTime) > pulseLength) {
+        m->newPulse = true;
+        m->iteration++;
+        if (m->iteration == repetitions) {
+          m->done = true;
+          allDone--;
+        }
+      }
+      else {
+        if (!revert && m->iteration % 2 == 1) {
+          continue;
+        }
+        else {
+          driveMagnet(i);
+        }
       }
     }
-    else {
-      runPulse *= -1;
-    }
+    i = (i + 1) % magnetsNumber;
   }
   for (int magnet = 0; magnet < magnetsNumber; magnet++) {
     stopMagnet(magnet);
@@ -293,17 +350,17 @@ void pulseMagnets(int pulseLength, int repetitions, int revert, int *magnetsToRu
 
 void driveMagnet(int magnet) {
   Magnet* m = &magnets[magnet];
-  if (canRun(magnet)) {
-    if (m->isRunning == false) {
-      m->isRunning = true;
-      if ( (millis() - m->lastRun) > minTimeBetweenRuns) {
-        m->firstRun = millis();
-      }
-    }
+  //  if (canRun(magnet)) {
+  //    if (m->isRunning == false) {
+  //      m->isRunning = true;
+  //      if ( (millis() - m->lastRun) > minTimeBetweenRuns) {
+  //        m->firstRun = millis();
+  //      }
+  //    }
 
-    digitalWrite(m->forwardPin, m->fieldDirection + 0.5);
-    digitalWrite(m->backwardPin, -m->fieldDirection + 0.5);
-  }
+  digitalWrite(m->forwardPin, m->fieldDirection + 0.5);
+  digitalWrite(m->backwardPin, -m->fieldDirection + 0.5);
+  //  }
 }
 
 
@@ -333,12 +390,12 @@ bool canRun(int magnet) {
 
 void stopMagnet(int magnet) {
   Magnet *m = &magnets[magnet];
-  if (m->isRunning) {
-    digitalWrite(m->forwardPin, 0 );
-    digitalWrite(m->backwardPin, 0);
-    m->lastRun = millis();
-    m->isRunning = false;
-  }
+  //  if (m->isRunning) {
+  digitalWrite(m->forwardPin, 0 );
+  digitalWrite(m->backwardPin, 0);
+  m->lastRun = millis();
+  m->isRunning = false;
+  //  }
 }
 
 bool certain (int magnet, int lower, int higher) {
@@ -377,6 +434,22 @@ void setDirection(int magnet, float dir) {
   m->fieldDirection = dir;
 }
 
+int getLetter(int *dots) {
+  bool match = true;
+  for (int i = 0; i < 26; i++) {
+    match = true;
+    for (int j = 0; j < magnetsNumber; j++) {
+      if (letters[i][j] != dots[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+}
+
 
 
 
@@ -388,6 +461,7 @@ int readHall(int magnet) {
   if (magnet >= magnetsNumber) {
     return 0;
   }
+
   int reading = analogRead(magnets[magnet].sensor.inputPin);
   return reading;
 }
--- keyboardController/keyboardController.pde
+++ keyboardController/keyboardController.pde
@@ -5,16 +5,16 @@ int character;
 char val;
 String file1 = "file1.txt";
 int mode = 9; // 0 = reading letters from keyboard
-              // 1 = prep - sending letters to keyboard
-              // 9 = setup
-              
-final int ESCSignal = 6;
+// 1 = prep - sending letters to keyboard
+// 9 = setup
+
+final int ESCSignal = -1;
 final int ConfirmationSignal = 5;
 final int PrepReadingMode = 1;
 final int ReadingMode = 2;
 final int WritingMode = 0;
 final int SetUpMode = 9;
-              
+
 //--------------- Signals from keyboard -------------
 //
 //  5 - confirmation
@@ -24,108 +24,118 @@ final int SetUpMode = 9;
 //
 // -------------- Letters for controller -------------
 //
-int ESC = 27;
+int ESC = 8;
 //
 //
 
-              
+
 int shift = 10;
 
-void setup(){
+void setup() {
   String portName = Serial.list()[0]; //change the 0 to a 1 or 2 etc. to match your port
   myPort = new Serial(this, portName, 9600);
-  reader = createReader(file1); 
-  
-  println("Choose to send letters to keyboard [r] or accept keyboard's input [w], [Esc] to exit any mode and start again.");  
+
+  println("Choose to send letters to keyboard [r] or accept keyboard's input [w], [Esc] to exit any mode and start again.");
 }
 
 void draw()
 {
 
-    if ( myPort.available() > 0) {  // If data is available,
-      val = myPort.readChar();         // read it and store it in val
-        switch (mode){
-         case PrepReadingMode: {
-           int intVal = (int)val;
-           if (intVal == ConfirmationSignal){
-             println("Choose the signal type: signal pulse 1 -> 3 Vibration");
-             mode = PrepReadingMode;
-           }
-         }
-         
-         case WritingMode: {
-           print(val);
-         }      
-         
-         case ReadingMode: {
-           print(val);
-         }
-          
+  if ( myPort.available() > 0) {  // If data is available,
+    val = myPort.readChar();         // read it and store it in val
+    switch (mode) {
+    case PrepReadingMode: 
+      {
+        int intVal = (int)val;
+        if (intVal == ConfirmationSignal) {
+          reader = createReader(file1);
+          println("Choose the signal type: signal pulse 1 -> 3 Vibration");
+          mode = PrepReadingMode;
         }
-        //println(val); //print it out in the console
+      }
+
+    case WritingMode: 
+      {
+      }      
+
+    case ReadingMode: 
+      {
+      }
+
+    case SetUpMode: 
+      {
+      }
     }
+    print(val);
+  }
 }
 
 
 void keyPressed() {
-  println("Pressed: " + key);
-  
+  println("Pressed: " + (int)key);
+
   // reset key [ESC]
-  if (key == ESC){
-      mode = SetUpMode;
-      myPort.write(SetUpMode);
+  if (key == ESC) {
+    mode = SetUpMode;
+    println("Received break key");
+    myPort.write(ESCSignal);
+    return;
   }
-  
+
   // Selecting mode
-  if(mode == SetUpMode){
-    if (key == 'w' || key == 'W'){
-      mode = WritingMode;
-      myPort.write(WritingMode);
-    }
-    else if (key == 'r' || key == 'R'){
-      mode = PrepReadingMode;
-      myPort.write(PrepReadingMode);
-    }
-    else return;
-  }
-  else if(mode == PrepReadingMode){
-    if (key < '3' && key > '1'){
-      myPort.write(key - '0'); 
-      mode = ReadingMode;
-    }
-  }
-  else if(mode == ReadingMode) {
-    if (key == ' '){
-      sendNextCharachter();
-    }
+  switch (mode) {
+    case SetUpMode:  
+      {
+        if (key == 'w' || key == 'W') {
+          mode = WritingMode;
+          myPort.write(WritingMode);
+        } else if (key == 'r' || key == 'R') {
+          mode = PrepReadingMode;
+          myPort.write(PrepReadingMode);
+        } else return;
+      }
+    case PrepReadingMode: 
+      {
+        if (key < '4' && key > '0') {
+          println("sending mode: " +(key - '1'));
+          myPort.write(key - '1'); 
+          mode = ReadingMode;
+        }
+      }
+    case ReadingMode: 
+      {
+        if (key == ' ') {
+          sendNextCharachter();
+        }
+      }
   }
 }
 
-void sendNextCharachter(){
-      try {
+void sendNextCharachter() {
+  try {
     character = reader.read();
-  } catch (IOException e) {
+  } 
+  catch (IOException e) {
     e.printStackTrace();
     character = 0;
   }
-  if (character == 0) {
-    println("File finished");
+  if (character == 0 || character == -1) {
+    println("File finished, " + ESCSignal);
+    myPort.write(ESCSignal);
     // Stop reading because of an error or file is empty
     mode = SetUpMode;
   } else {
-      int toSend = 0;
-      if (character >= 'a' && character <= 'z'){
-        toSend = character - 'a' + shift;
-      }
-      else if (character >= 'A' && character <= 'Z'){
-        toSend = character - 'A' + shift;
-      }
-      if (toSend != 0){
-        println("Sending: " +(char) character);
-        myPort.write(toSend);
-      }
-      else {
-        sendNextCharachter();
-      }
-    } 
+    int toSend = 0;
+    if (character >= 'a' && character <= 'z') {
+      toSend = character - 'a' + shift;
+    } else if (character >= 'A' && character <= 'Z') {
+      toSend = character - 'A' + shift;
+    }
+    if (toSend != 0) {
+      println("Sending: " +(char) character);
+      myPort.write(toSend);
+    } else {
+      sendNextCharachter();
+    }
+  }
 }
